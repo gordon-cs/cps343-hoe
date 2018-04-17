d7e3fe74cb0de34ac444bac1af3a586a9fd91fe4
Jonathan Senning <jonathan.senning@gmail.com>
2018-04-17 10:27:26 -0400

    updated data generation script

--- 10-cuda-shmem/collect_data.sh
+++ 10-cuda-shmem/collect_data.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 arch=$(deviceQuery | grep "Device 0" | cut -d: -f2 | sed -e 's/[ "]//g')
+types="Global Shared"
+sizes="08 16 32"
+dim0=1580
+dim1=1620
 
-for k in 08 16 32
+for k in ${sizes}
 do
-    for ((n=1580;n<=1620;n++))
+    # clear out any previous versions of files we will build
+    for t in ${types}
     do
-	./matmul$k $n
-    done | tee data.${arch}.$k
-    for t in Global Shared
+	rm -f $t.${arch}.$k
+    done
+
+    # sweep through a range of matrix dimesions
+    for ((n=dim0;n<=dim1;n++))
     do
-	grep $t data.${arch}.$k | awk '{print $5}' > $t.${arch}.$k
+	./matmul$k $n | tee tmp.$$
+	for t in ${types}
+	do
+	    echo "$n $(grep $t tmp.$$ | awk '{print $5}')" >> $t.${arch}.$k
+	done
     done
 done
+
+# clean up
+rm -f tmp.$$
