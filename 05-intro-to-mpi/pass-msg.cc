6937e77233336984fec04e1eba82036f9d89fa6d
Jonathan Senning <jonathan.senning@gmail.com>
2022-02-08 16:26:03 -0500

    updated MPI pass-msg program

--- 05-intro-to-mpi/pass-msg.cc
+++ 05-intro-to-mpi/pass-msg.cc
@@ -5,28 +5,27 @@
 
 int main( int argc, char* argv[] )
 {
-    int rank;
-    int numberOfProcesses;
+    int my_rank;
+    int num_proc;
     int msg;
     const int tag = 42; // the answer to the ultimate question
     MPI_Status status;
 
     // Initalize MPI
     MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &numberOfProcesses );
+    MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
+    MPI_Comm_size( MPI_COMM_WORLD, &num_proc );
 
     // Set who I'll be receive from (if anyone) and sending to (if anyone)
-    const int prev = rank - 1;
-    const int next = rank + 1;
+    const int prev = my_rank - 1;
+    const int next = my_rank + 1;
 
-    if ( rank == 0 )
+    if ( my_rank == 0 )
     {
 	// Rank 0 process is the master; it starts the token by sending it
 	// to the rank 1 process.
-	msg = 1001;
-        printf( "Process %d sending message %d to process %d\n",
-                rank, msg, next );
+	msg = 1000;
+        printf( "Process %d sending %d to process %d\n", my_rank, msg, next );
 	MPI_Send( &msg, 1, MPI_INT, next, tag, MPI_COMM_WORLD );
     }
     else
@@ -35,16 +34,16 @@ int main( int argc, char* argv[] )
 	// from our predecessor, increment it, and send it along to our
 	// successor, if any.
 	MPI_Recv( &msg, 1, MPI_INT, prev, tag, MPI_COMM_WORLD, &status );
-        printf( "Process %d received %d\n", rank, msg );
+        printf( "Process %d received %d\n", my_rank, msg );
 
         // Increment message so we now it's been here
 	msg++;
 
         // If we're not the last process, send the message on...
-        if ( rank < numberOfProcesses - 1 )
+        if ( my_rank < num_proc - 1 )
         {
-            printf( "Process %d sending message %d to process %d\n",
-                    rank, msg, next );
+            printf( "Process %d sending %d to process %d\n",
+                    my_rank, msg, next );
             MPI_Send( &msg, 1, MPI_INT, next, tag, MPI_COMM_WORLD );
         }
     }
