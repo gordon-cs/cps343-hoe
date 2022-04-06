98e5098f0d7dc6089ba1e274b61a017f1d300372
Jonathan Senning <jonathan.senning@gmail.com>
2022-04-06 11:37:58 -0400

    modified collect_data script to use srun

--- 09-cuda-shmem/collect_data.sh
+++ 09-cuda-shmem/collect_data.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 PATH=/shared/cuda-samples/bin/x86_64/linux/release:$PATH
-arch=$(deviceQuery | grep "Device 0" | cut -d: -f2 | sed -e 's/[ "]//g')
-types="Global Shared"
+gpus="QuadroP1000 QuadroRTX3000"
 sizes="08 16 32"
+types="Global Shared"
 dim0=1580
 dim1=1620
 
-for k in ${sizes}
+for gpu in ${gpus}
 do
-    # clear out any previous versions of files we will build
-    for t in ${types}
+    for k in ${sizes}
     do
-	rm -f $t.${arch}.$k
-    done
-
-    # sweep through a range of matrix dimesions
-    for ((n=dim0;n<=dim1;n++))
-    do
-	./matmul$k $n | tee tmp.$$
+	# clear out any previous versions of files we will build
 	for t in ${types}
 	do
-	    echo "$n $(grep $t tmp.$$ | awk '{print $5}')" >> $t.${arch}.$k
+	    rm -f $t.${gpu}.$k
+	done
+
+	# sweep through a range of matrix dimesions
+	for ((n=dim0;n<=dim1;n++))
+	do
+	    srun --gres=gpu:${gpu} ./matmul$k $n | tee tmp.$$
+	    for t in ${types}
+	    do
+		echo "$n $(grep $t tmp.$$ | awk '{print $5}')" >> $t.${gpu}.$k
+	    done
 	done
     done
 done
