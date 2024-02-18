b8bb130096d54e01cda6c8abc08c72e01f779f3f
Jonathan Senning <jonathan.senning@gmail.com>
2024-02-18 15:26:44 -0500

    Removed extra whitespace between parentheses

--- 05-intro-to-mpi/hello.cc
+++ 05-intro-to-mpi/hello.cc
@@ -4,7 +4,7 @@
 #include <cstdio>
 #include <mpi.h>
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
     using namespace std;
 
@@ -14,26 +14,26 @@ int main( int argc, char* argv[] )
     int tag = 99;       // message tag
     MPI_Status status;  // message receive status
 
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &num_proc );
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
 
-    if ( my_rank == 0 )
+    if (my_rank == 0)
     {
-        printf( "Hello from Rank %2d: There are %d processes\n",
-                my_rank, num_proc );
+        printf("Hello from Rank %2d: There are %d processes\n",
+                my_rank, num_proc);
 	// send a message to each non-rank 0 process
-	for ( int i = 1; i < num_proc; i++ )
+	for (int i = 1; i < num_proc; i++)
 	{
 	    msg = 100 * i;
-	    MPI_Send( &msg, 1, MPI_INT, i, tag, MPI_COMM_WORLD );
+	    MPI_Send(&msg, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
 	}
     }
     else
     {
 	// receive message from rank 0 process
-	MPI_Recv( &msg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status );
-        printf( "Hello from Rank %2d: Received %d\n", my_rank, msg );
+	MPI_Recv(&msg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+        printf("Hello from Rank %2d: Received %d\n", my_rank, msg);
     }
 
     MPI_Finalize();
--- 05-intro-to-mpi/pass-msg.cc
+++ 05-intro-to-mpi/pass-msg.cc
@@ -3,7 +3,7 @@
 #include <cstdio>
 #include <mpi.h>
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
     int my_rank;
     int num_proc;
@@ -12,39 +12,39 @@ int main( int argc, char* argv[] )
     MPI_Status status;
 
     // Initalize MPI
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &num_proc );
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
 
     // Determine who I'll receive from (if anyone) and send to (if anyone)
     const int prev = my_rank - 1;
     const int next = my_rank + 1;
 
-    if ( my_rank == 0 )
+    if (my_rank == 0)
     {
         // We are the Rank 0 process so we start things off by sending the
         // token to the rank 1 process
 	msg = 1000;
-        printf( "Process %d sending %d to process %d\n", my_rank, msg, next );
-	MPI_Send( &msg, 1, MPI_INT, next, tag, MPI_COMM_WORLD );
+        printf("Process %d sending %d to process %d\n", my_rank, msg, next);
+	MPI_Send(&msg, 1, MPI_INT, next, tag, MPI_COMM_WORLD);
     }
     else
     {
 	// We're not the rank 0 process so we wait for the token to arrive
 	// from our predecessor, increment it, and send it along to our
 	// successor (if any).
-	MPI_Recv( &msg, 1, MPI_INT, prev, tag, MPI_COMM_WORLD, &status );
-        printf( "Process %d received %d\n", my_rank, msg );
+	MPI_Recv(&msg, 1, MPI_INT, prev, tag, MPI_COMM_WORLD, &status);
+        printf("Process %d received %d\n", my_rank, msg);
 
         // Increment message (so we know it's been here)
 	msg++;
 
         // If we're not the last process, send the message on...
-        if ( my_rank < num_proc - 1 )
+        if (my_rank < num_proc - 1)
         {
-            printf( "Process %d sending %d to process %d\n",
-                    my_rank, msg, next );
-            MPI_Send( &msg, 1, MPI_INT, next, tag, MPI_COMM_WORLD );
+            printf("Process %d sending %d to process %d\n",
+                    my_rank, msg, next);
+            MPI_Send(&msg, 1, MPI_INT, next, tag, MPI_COMM_WORLD);
         }
     }
 
