cef4a8f9282aa02633b28cc499c9d12f42759c56
Jonathan Senning <jonathan.senning@gmail.com>
2024-03-19 18:12:59 -0400

    make command-line switch for detailed timing information

--- 07-parallel-sorting/psrs_radixsort.cc
+++ 07-parallel-sorting/psrs_radixsort.cc
@@ -1,11 +1,12 @@
 /*
- * $Smake: mpic++ -DSHOW_TIMING_DATA -O3 -o %F %f
+ * $Smake: mpic++ -O3 -o %F %f
  *
  * Copyright (c) 2010 Jonathan Senning <jonathan.senning@gordon.edu.
  * Department of Mathematics and Computer Science
  * Gordon College, Wenham MA 01984-1899
  * Revised March 2018 - display communication and computation timing data
- * Revised March 2022 - Created radix sort version 
+ * Revised March 2022 - Created radix sort version
+ * Revised March 2024 - Added command-line switch for timing information
  *
  * Implements Parallel Sorting with Regular Sampling (PSRS) using a
  * sequential Radix Sort for local sorting.  Radix sort implementation
@@ -16,9 +17,8 @@
 #include <iomanip>
 #include <fstream>
 #include <cstdlib>
+#include <unistd.h>
 #include <mpi.h>
-
-//#define SHOW_TIMING_DATA
 
 //----------------------------------------------------------------------------
 //
@@ -39,9 +39,9 @@
 // subintervals of roughly the same size.  The following three
 // pairs of calls find the starting and ending indices of each
 // subinterval:
-//   decompose1d( 100, 3, 0, &s, &e );  (now s =  0, e = 33)
-//   decompose1d( 100, 3, 1, &s, &e );  (now s = 34, e = 66)
-//   decompose1d( 100, 3, 2, &s, &e );  (now s = 67, e = 99)
+//   decompose1d(100, 3, 0, &s, &e);  (now s =  0, e = 33)
+//   decompose1d(100, 3, 1, &s, &e);  (now s = 34, e = 66)
+//   decompose1d(100, 3, 2, &s, &e);  (now s = 67, e = 99)
 //
 // The subinterval length can be computed with e - s + 1.
 //
@@ -50,13 +50,13 @@
 // "Using MPI" by Gropp et al.  It has been adapted to use
 // 0-based indexing.
 
-static void decompose1d( int n, int m, int i, int* s, int* e )
+static void decompose1d(int n, int m, int i, int* s, int* e)
 {
     const int length  = n / m;
     const int deficit = n % m;
-    *s =  i * length + ( i < deficit ? i : deficit );
-    *e = *s + length - ( i < deficit ? 0 : 1 );
-    if ( ( *e >= n ) || ( i == m - 1 ) ) *e = n - 1;
+    *s =  i * length + (i < deficit ? i : deficit);
+    *e = *s + length - (i < deficit ? 0 : 1);
+    if ((*e >= n) || (i == m - 1)) *e = n - 1;
 }
 
 //-----------------------------------------------------------------------------
@@ -77,13 +77,13 @@ static void decompose1d( int n, int m, int i, int* s, int* e )
 //
 // Derived from code found at http://www.cubic.org/docs/radix.htm.
 
-void countingsort( size_t offset, size_t n, int *source, int *dest )
+void countingsort(size_t offset, size_t n, int *source, int *dest)
 {
     // to change the data type of the list to something other than int,
     // the type of pointers in the function declaration must be changed
     // and the argument of the sizeof() function and the type of the
     // *sp pointer must be changed in the next two lines.
-    const unsigned int datasize = sizeof( int );
+    const unsigned int datasize = sizeof(int);
     int* sp;                    // pointer into source array
 
     unsigned int count[256];    // holds freq. of bytes then location of bytes
@@ -93,11 +93,11 @@ void countingsort( size_t offset, size_t n, int *source, int *dest )
     unsigned int sum, tmp;      // temporary variables
 
     // zero out count[] array
-    for ( i = 0, cp = count; i < 256; i++ ) *cp++ = 0;
+    for (i = 0, cp = count; i < 256; i++) *cp++ = 0;
 
     // set count[i] equal to the number of elements with bytes equal to i
     bp = (unsigned char *) source + offset;
-    for ( i = 0; i < n; i++ ) {
+    for (i = 0; i < n; i++) {
 	cp = count + *bp;
 	(*cp)++;
 	bp += datasize;
@@ -105,7 +105,7 @@ void countingsort( size_t offset, size_t n, int *source, int *dest )
 
     // reset count[i] to be equal to the number of elements with bytes
     // less than or equal to i
-    for ( i = 0, sum = 0, cp = count; i < 256; i++ ) {
+    for (i = 0, sum = 0, cp = count; i < 256; i++) {
 	tmp = *cp;
 	*cp++ = sum;
 	sum += tmp;
@@ -114,7 +114,7 @@ void countingsort( size_t offset, size_t n, int *source, int *dest )
     // place values in proper place in dest array
     bp = (unsigned char *) source + offset;
     sp = source;
-    for ( i = 0; i < n; i++ ) {
+    for (i = 0; i < n; i++) {
 	cp = count + *bp;
 	dest[*cp] = *sp++;
 	(*cp)++;
@@ -136,13 +136,13 @@ void countingsort( size_t offset, size_t n, int *source, int *dest )
 //
 // Derived from code found at http://www.cubic.org/docs/radix.htm.
 
-void radixsort( int *source, unsigned int n )
+void radixsort(int *source, unsigned int n)
 {
     int* temp = new int [n];
-    countingsort( 0, n, source, temp );
-    countingsort( 1, n, temp, source );
-    countingsort( 2, n, source, temp );
-    countingsort( 3, n, temp, source );
+    countingsort(0, n, source, temp);
+    countingsort(1, n, temp, source);
+    countingsort(2, n, source, temp);
+    countingsort(3, n, temp, source);
     delete [] temp;
 }
 
@@ -154,14 +154,15 @@ void radixsort( int *source, unsigned int n )
 //    int  inLen         - length of initial list
 //    int  master        - rank of MPI master process (determines splitters)
 //    MPI_Comm comm      - MPI communicator common to all sorting processes
+//    bool doTiming      - true for timing information
 // Output:
 //    int* outList       - pointer to first element of output (sorted) list
 //    int  outLen        - length of output (sorted) list
 // Returns:
 //    0  if success; -1 to indicate error
 
-int psrsSort( int* inList, int inLen, int* outList, int* outLen, int master,
-	      MPI_Comm comm )
+int psrsSort(int* inList, int inLen, int* outList, int* outLen, int master,
+	     MPI_Comm comm, bool doTiming)
 {
     enum { SPLITTER, BIN };
     int rank;              // process id
@@ -174,33 +175,49 @@ int psrsSort( int* inList, int inLen, int* outList, int* outLen, int master,
     int  n, m;             // temporary variables array indices
 
     // get communicator parameters
-    MPI_Comm_rank( comm, &rank );
-    MPI_Comm_size( comm, &size );
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    if (doTiming)
+    {
+        if (rank == 0)
+        {
+            printf("\n");
+            printf("                broad-     all-            ");
+            printf("    pre-   split-    post-      comm\n");
+            printf("rank   gather     cast   gather sendrecv   ");
+            printf("    sort     list     sort     fract\n");
+            printf("---- -------- -------- -------- --------   ");
+            printf("-------- -------- --------   -------\n");
+        }
+        fflush(stdout);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
 
     // sort original list
     double t0 = MPI_Wtime();
-    radixsort( inList, inLen );
+    radixsort(inList, inLen);
     double time_sort1 = MPI_Wtime() - t0;
 
     // collect set of regularly-spaced samples from sorted list and
     // gather the samples from all processes on master process in array
     sample = new int [size];
-    samples = ( rank == master ? new int [size * size] : NULL );
-    for ( int i = 0; i < size; i++ ) sample[i] = inList[( i * inLen ) / size];
+    samples = (rank == master ? new int [size * size] : NULL);
+    for (int i = 0; i < size; i++) sample[i] = inList[(i * inLen) / size];
     t0 = MPI_Wtime();
-    MPI_Gather( sample, size, MPI_INT, samples, size, MPI_INT, master, comm );
+    MPI_Gather(sample, size, MPI_INT, samples, size, MPI_INT, master, comm);
     double time_gather = MPI_Wtime() - t0;
 
     // master process sorts list of samples to determine splitter values
     // and broadcasts lists of splitters to all processes
     splitter = new int [size - 1];
-    if ( rank == master )
+    if (rank == master)
     {
-	radixsort( samples, size * size );
-	for ( int i = 1; i < size; i++ ) splitter[i - 1] = samples[size * i];
+	radixsort(samples, size * size);
+	for (int i = 1; i < size; i++) splitter[i - 1] = samples[size * i];
     }
     t0 = MPI_Wtime();
-    MPI_Bcast( splitter, size - 1, MPI_INT, master, comm );
+    MPI_Bcast(splitter, size - 1, MPI_INT, master, comm);
     double time_broadcast = MPI_Wtime() - t0;
     delete [] samples;
     delete [] sample;
@@ -210,16 +227,15 @@ int psrsSort( int* inList, int inLen, int* outList, int* outLen, int master,
     binSize = new int [size];
     t0 = MPI_Wtime();
     n = m = 0;
-    for ( int i = 0; i < size - 1; i++ )
+    for (int i = 0; i < size - 1; i++)
     {
-	while ( m < inLen && inList[m] < splitter[i] ) m++;
+	while (m < inLen && inList[m] < splitter[i]) m++;
 	binSize[i] = m - n;
 	n = m;
     }
     binSize[size - 1] = inLen - n;
     double time_split = MPI_Wtime() - t0;
     delete [] splitter;
-    
 
     // distribute all bin size information to all processes and
     // determine length of list needed to hold all data this process
@@ -227,22 +243,22 @@ int psrsSort( int* inList, int inLen, int* outList, int* outLen, int master,
     // that process i will send to process j.
     binSizes = new int [size * size];
     t0 = MPI_Wtime();
-    MPI_Allgather( binSize, size, MPI_INT, binSizes, size, MPI_INT, comm );
+    MPI_Allgather(binSize, size, MPI_INT, binSizes, size, MPI_INT, comm);
     double time_allgather = MPI_Wtime() - t0;
     n = 0;
-    for ( int i = 0; i < size; i++ ) n += binSizes[i * size + rank];
-    if ( *outLen < n ) return -1;
+    for (int i = 0; i < size; i++) n += binSizes[i * size + rank];
+    if (*outLen < n) return -1;
     *outLen = n;
 
     // exchange partial bins with other processes.
     n = m = 0;
     t0 = MPI_Wtime();
-    for ( int i = 0; i < size; i++ )
+    for (int i = 0; i < size; i++)
     {
         int j = i * size + rank;
-	MPI_Sendrecv( &inList[n],  binSize[i],  MPI_INT, i, BIN,
-		      &outList[m], binSizes[j], MPI_INT, i, BIN,
-		      comm, MPI_STATUS_IGNORE );
+	MPI_Sendrecv(&inList[n],  binSize[i],  MPI_INT, i, BIN,
+                     &outList[m], binSizes[j], MPI_INT, i, BIN,
+                     comm, MPI_STATUS_IGNORE);
 	n += binSize[i];
 	m += binSizes[j];
     }
@@ -251,126 +267,131 @@ int psrsSort( int* inList, int inLen, int* outList, int* outLen, int master,
 
     // finally, sort received data
     t0 = MPI_Wtime();
-    radixsort( outList, *outLen );
+    radixsort(outList, *outLen);
     double time_sort2 = MPI_Wtime() - t0;
 
     // compute elapsed communication and sorting time
     double tcomm = time_gather+time_broadcast+time_allgather+time_sendrecv;
     double tcomp = time_sort1 + time_sort2 + time_split;
-    double ratio = tcomm / ( tcomm + tcomp );
-#if defined(SHOW_TIMING_DATA)
-    printf( "%4d %8.5f %8.5f %8.5f %8.5f | %8.5f %8.5f %8.5f | %7.4f\n",
-            rank,
-            time_gather, time_broadcast, time_allgather, time_sendrecv,
-            time_sort1, time_split, time_sort2, ratio );
-#endif
-    
+    double ratio = tcomm / (tcomm + tcomp);
+
+    // display detailed timing information if requested
+    if (doTiming)
+    {
+        printf("%4d %8.5f %8.5f %8.5f %8.5f | %8.5f %8.5f %8.5f | %7.4f\n",
+               rank,
+               time_gather, time_broadcast, time_allgather, time_sendrecv,
+               time_sort1, time_split, time_sort2, ratio);
+    }
+
     return 0;
 }
 
 //-----------------------------------------------------------------------------
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-    int rank;            // process id
-    int size;            // number of processes
-    int nelem;           // number of elements in global list
-    int n = 0;           // number of elements in local list
-    int outLen;          // number of elements in local sorted list
-    int start;           // starting value for range
-    int end;             // ending value for range
-    int* list = NULL;    // pointer to local portion of unsorted list
-    int* outList = NULL; // pointer to local portion of sorted list;
-    double t0, t1;       // timing variables
-    int status;
+    bool doTiming = false; // show detailed timing information
+    int rank;              // process id
+    int size;              // number of processes
+    int nelem;             // number of elements in global list
+    int n = 0;             // number of elements in local list
+    int outLen;            // number of elements in local sorted list
+    int start;             // starting value for range
+    int end;               // ending value for range
+    int* list = NULL;      // pointer to local portion of unsorted list
+    int* outList = NULL;   // pointer to local portion of sorted list;
+    double t0, t1;         // timing variables
+    int status;            // sorting status (0 means okay)
+
+    int opt;
+    while ((opt = getopt(argc, argv, "t")) != -1)
+    {
+        switch (opt)
+        {
+            case 't':
+                doTiming = true;
+                break;
+            default:
+                fprintf(stderr, "usage: %s [-t] N\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    argv[optind - 1] = argv[0];
+    argc -= (optind - 1);
+    argv += (optind - 1);
 
     // initalize MPI
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // initalize random number generator
-    srandom( (unsigned int) ( time( NULL ) * rank ) );
+    srandom((unsigned int) (time(NULL) * rank));
 
     // get total list length from command line
-    if ( argc != 2 )
+    if (argc != 2)
     {
-        fprintf( stderr, "usage: %s N\n", argv[0] );
-	exit( EXIT_FAILURE );
+        fprintf(stderr, "usage: %s [-t] N\n", argv[0]);
+	exit(EXIT_FAILURE);
     }
-    nelem = atol( argv[1] );
-    if ( nelem <= 0 )
+    nelem = atol(argv[1]);
+    if (nelem <= 0)
     {
-	fprintf( stderr, "Error: list length N must be positive\n" );
-	exit( EXIT_FAILURE );
+	fprintf(stderr, "Error: list length N must be positive\n");
+	exit(EXIT_FAILURE);
     }
 
     // determine the length of the local portion of the list
-    decompose1d( nelem, size, rank, &start, &end );
+    decompose1d(nelem, size, rank, &start, &end);
     n = end - start + 1;
 
     // generate list of random numbers
     list = new int [n];
-    for ( int i = 0; i < n; i++ ) list[i] = random();
+    for (int i = 0; i < n; i++) list[i] = random();
 
     // allocate memory for sorted list
     outLen = 2 * n;
     outList = new int [outLen];
 
-#if defined(SHOW_TIMING_DATA)
-    if ( rank == 0 )
-    {
-        printf( "\n" );
-        printf( "                broad-     all-            " );
-        printf( "    pre-   split-    post-      comm\n" );
-        printf( "rank   gather     cast   gather sendrecv   " );
-        printf( "    sort     list     sort     fract\n" );
-        printf( "---- -------- -------- -------- --------   " );
-        printf( "-------- -------- --------   -------\n" );
-    }
-    fflush( stdout );
-    MPI_Barrier( MPI_COMM_WORLD );
-#endif
-
     // sort the list
     t0 = MPI_Wtime();
-    status = psrsSort( list, n, outList, &outLen, 0, MPI_COMM_WORLD );
+    status = psrsSort(list, n, outList, &outLen, 0, MPI_COMM_WORLD,
+                      doTiming);
     t1 = MPI_Wtime();
 
-    if ( status < 0 ) fprintf( stderr, "Error in psrsSort()\n" );
+    if (status < 0) fprintf(stderr, "Error in psrsSort()\n");
 
     // display sorted list if it is fairly short
-    if ( nelem <= 100 )
+    if (nelem <= 100)
     {
-	for ( int r = 0; r < size; r++ )
+	for (int r = 0; r < size; r++)
 	{
-	    if ( rank == r )
+	    if (rank == r)
 	    {
-		for ( int i = 0; i < outLen; i++ )
+		for (int i = 0; i < outLen; i++)
 		{
-                    printf( "rank %2d: list[%3d] = %12d\n",
-                            rank, i, outList[i] );
+                    printf("rank %2d: list[%3d] = %12d\n",
+                           rank, i, outList[i]);
 		}
-                fflush( stdout );
+                fflush(stdout);
 	    }
-	    MPI_Barrier( MPI_COMM_WORLD );
+	    MPI_Barrier(MPI_COMM_WORLD);
 	}
     }
 
-    fflush( stdout );
-    MPI_Barrier( MPI_COMM_WORLD );
+    MPI_Finalize();
 
     // report sort time
-    if ( rank == 0 )
+    if (rank == 0)
     {
-        printf( "\n%12d element list sorted in %12.8f seconds\n", 
-                nelem, t1 - t0 );
+        printf("\n%12d element list sorted in %12.8f seconds\n", 
+               nelem, t1 - t0);
     }
 
     // all done; clean up
     delete [] list;
     delete [] outList;
-    MPI_Finalize();
 
     return 0;
 }
